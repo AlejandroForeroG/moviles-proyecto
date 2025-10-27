9c1cd2dda7dbb0e5248ecc588b0990bbe7b9bae0
YesidMarinUni <y.marinr@uniandes.edu.co>
2025-10-27 16:17:32 -0500

    Actualizar componentes Test

--- app/src/test/java/com/proyecto/uniandes/vynils/di/TestAlbumModule.kt
+++ app/src/test/java/com/proyecto/uniandes/vynils/di/TestAlbumModule.kt
@@ -17,36 +17,43 @@ import javax.inject.Singleton
 )
 object TestAlbumModule {
 
+    private val albums = mutableListOf(
+        ResponseAlbum(
+            id = 1,
+            name = "Buscando América",
+            cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg"
+        ),
+        ResponseAlbum(
+            id = 2,
+            name = "Poeta del pueblo",
+            cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg"
+        ),
+        ResponseAlbum(
+            id = 3,
+            name = "A Day at the Races",
+            cover = "https://upload.wikimedia.org/wikipedia/en/7/79/A_Day_at_the_Races_%28album%29.jpg"
+        )
+    )
+
+    private var nextId = 4
+
     @Provides
     @Singleton
     fun provideFakeVinylApiService(): VinylApiService {
         return object : VinylApiService {
             override suspend fun getAllAlbums(): Response<List<ResponseAlbum>> {
-                // Return mock albums with predictable data
-                val albums = listOf(
-                    ResponseAlbum(
-                        id = 1,
-                        name = "Buscando América",
-                        cover = "https://i.pinimg.com/564x/aa/5f/ed/aa5fed7fac61cc8f41d1e79db917a7cd.jpg"
-                    ),
-                    ResponseAlbum(
-                        id = 2,
-                        name = "Poeta del pueblo",
-                        cover = "https://cdn.shopify.com/s/files/1/0275/3095/products/image_4931268b-7acf-4702-9c55-b2b3a03ed999_1024x1024.jpg"
-                    ),
-                    ResponseAlbum(
-                        id = 3,
-                        name = "A Day at the Races",
-                        cover = "https://upload.wikimedia.org/wikipedia/en/7/79/A_Day_at_the_Races_%28album%29.jpg"
-                    )
-                )
-                return Response.success(albums)
+                return Response.success(albums.toList())
             }
 
             override suspend fun createAlbum(album: RequestAlbum): Response<ResponseAlbum> {
-                TODO("Not yet implemented")
+                val created = ResponseAlbum(
+                    id = nextId++,
+                    name = album.name,
+                    cover = album.cover
+                )
+                albums.add(created)
+                return Response.success(created)
             }
         }
     }
 }
-
